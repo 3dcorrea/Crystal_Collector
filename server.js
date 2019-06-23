3e6f2acd3fd58502c0d35a9ddd3d279983d6c22b
Dan Correa <dancorrea3@gmail.com>
2019-06-23 19:34:52 -0400

    moved files into app folder

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("routes/public"));
 
 
 // Routes
-require("./routes/htmlRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
 
 
 // If running a test, set syncOptions.force to true
